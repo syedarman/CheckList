922a323dc9b5851fcc50781be42a9c13fa96c681
Syed Arman <syedarman3397@gmail.com>
2022-11-21 21:20:17 +0530

    for controls

--- checklist/src/app/app.component.ts
+++ checklist/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'checklist';
+  constructor(private formBuilder : FormBuilder){}
+  form = this.formBuilder.group({
+    items :  this.formBuilder.array([this.getItem()], Validators.required)
+  });
+  
+
+  addItems(){
+    this.form.controls.items.push(this.getItem());
+  }
+
+  getItem(){
+    return this.formBuilder.group({
+      name:[null, Validators.required],
+      quantity:[null, Validators.required],
+      unit:[null, Validators.required]
+    })
+  }
+
+  saveItems(){
+    console.log(this.form.value);
+  }
 }
